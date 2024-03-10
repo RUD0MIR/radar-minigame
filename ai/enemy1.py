8057473438337f7fd1381901f6836ec470f02f37
RUD0MIR <rudomir115@gmail.com>
2024-03-10 11:02:23 +0300

    working fov-wall intersection

--- ai/enemy1.py
+++ ai/enemy1.py
@@ -4,9 +4,12 @@ from pathfinding.core.grid import Grid
 from pathfinding.core.node import GridNode
 from pathfinding.finder.a_star import AStarFinder
 
+from ai.map import Walls
+from ai.player import Player
+
 
 class Enemy1(pygame.sprite.Sprite):
-    def __init__(self, matrix, default_pos):
+    def __init__(self, matrix, default_pos, walls: Walls, screen):
 
         # basic
         super().__init__()
@@ -16,9 +19,13 @@ class Enemy1(pygame.sprite.Sprite):
         self.image.fill('blue')
         self.rect = pygame.Rect(default_pos, self.size)
 
+        self.fov_radius = 20
+        self.walls = walls
+        self.screen = screen
+
         # movement
         self.pos = self.rect.center
-        self.speed = 1.2
+        self.speed = 0.5
         self.direction = pygame.math.Vector2(0, 0)
 
         # path
@@ -32,6 +39,29 @@ class Enemy1(pygame.sprite.Sprite):
     def empty_path(self):
         self.path = []
 
+    @staticmethod
+    def calculate_h_value(start, end):
+        return ((start[0] - end[0]) ** 2 + (start[1] - end[1]) ** 2) ** 0.5
+
+    #TODO ->
+    def intersect_walls(self):
+        if self.path:
+            points = []
+            for point in self.path:
+                x = (point.x * self.cell_size) + self.cell_size // 2
+                y = (point.y * self.cell_size) + self.cell_size // 2
+                points.append((x, y))
+
+            intersect = False
+            # check fov intersection
+            for wall in self.walls:
+                intersect = wall.rect.clipline(points[0], points[-1])
+
+            if intersect:
+                pygame.draw.line(self.screen, 'green', points[0], points[-1])
+            else:
+                pygame.draw.line(self.screen, 'blue', points[0], points[-1])
+
     # call on rays pulse
     def find_path(self, player_pos):
         # start
@@ -42,12 +72,15 @@ class Enemy1(pygame.sprite.Sprite):
         end_x, end_y = player_pos[0] // self.cell_size, player_pos[1] // self.cell_size
         end = self.grid.node(end_x, end_y)
 
-        # path
-        finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-        self.path = finder.find_path(start, end, self.grid)[0]
-        self.grid.cleanup()
-        self.create_path_points_rects()
-        self.get_direction()
+        h = self.calculate_h_value((start_x, start_y), (end_x, end_y))
+        if h <= self.fov_radius:
+            # path
+            finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+            self.path = finder.find_path(start, end, self.grid)[0]
+
+            self.grid.cleanup()
+            self.create_path_points_rects()
+            self.get_direction()
 
     def draw_path(self, screen):
         if self.path:
@@ -58,6 +91,8 @@ class Enemy1(pygame.sprite.Sprite):
                 points.append((x, y))
 
             pygame.draw.lines(screen, 'red', False, points, 5)
+
+        self.intersect_walls()
 
     def create_path_points_rects(self):
         if self.path:
@@ -89,7 +124,9 @@ class Enemy1(pygame.sprite.Sprite):
         else:
             self.empty_path()
 
-    def update(self):
+    def update(self, player: Player):
         self.pos += self.direction * self.speed
         self.path_point_reached()
         self.rect.center = self.pos
+
+        self.find_path((player.rect.x, player.rect.y))
--- ai/main.py
+++ ai/main.py
@@ -39,7 +39,7 @@ class Game:
         self.player = Player((550, 100), self.walls)
 
         # self.enemies = Enemies(self.player, self.walls, self.screen)
-        self.enemy = Enemy1(test_pathfinding_grid, (50, 280))
+        self.enemy = Enemy1(test_pathfinding_grid, (50, 280), self.walls, self.screen)
 
     def run(self):
         while self.running:
@@ -51,7 +51,7 @@ class Game:
             )
 
             self.screen.blit(self.enemy.image, self.enemy.rect)
-            self.enemy.update()
+            self.enemy.update(self.player)
             self.enemy.draw_path(self.screen)
 
             self.walls.draw(self.screen)
